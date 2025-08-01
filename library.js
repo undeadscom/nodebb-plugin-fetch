d73be4894739f57657ee6c9f09a8e28d323e423b
Maxim <m.matsuk@applicaster.com>
2025-08-01 17:51:47 +0300

    popover

--- library.js
+++ library.js
@@ -18,6 +18,59 @@ Plugin.addExternalData = async function (data) {
 
     data.templateValues.rates.percentageChange24h =
       json.data.percentageChange24h.toFixed(2);
+
+    // data for trade popover
+
+    data.templateValues.popoverData = [
+      {
+        name: "Gate.io",
+        link: "https://www.gate.io/en/trade/UDS_USDT",
+        description: "UDS / USDT",
+        icon: "https://cdn.undeads.com/assets/forum/currencies-bar/gate-io.png",
+      },
+      {
+        name: "MEXC",
+        link: "https://www.mexc.com/exchange/UDS_USDT",
+        description: "UDS / USDT",
+        icon: "https://cdn.undeads.com/assets/forum/currencies-bar/mexc.png",
+      },
+      {
+        name: "WEEX",
+        link: "https://www.weex.com/trade/UDS-USDT",
+        description: "UDS / USDT",
+        icon: "https://cdn.undeads.com/assets/forum/currencies-bar/weex.png",
+      },
+      {
+        name: "COINSTORE",
+        link: "https://www.coinstore.com/spot/udsusdt",
+        description: "UDS / USDT",
+        icon: "https://cdn.undeads.com/assets/forum/currencies-bar/coinstore.png",
+      },
+      {
+        name: "Biconomy.com",
+        link: "https://www.biconomy.com/exchange/UDS_USDT",
+        description: "UDS / USDT",
+        icon: "https://cdn.undeads.com/assets/forum/currencies-bar/biconomy.png",
+      },
+      {
+        name: "BingX",
+        link: "https://bingx.com/en/spot/UDSUSDT",
+        description: "UDS / USDT",
+        icon: "https://cdn.undeads.com/assets/forum/currencies-bar/bingx.png",
+      },
+      {
+        name: "XT.COM",
+        link: "https://www.xt.com/en/trade/uds_usdt",
+        description: "UDS / USDT",
+        icon: "https://cdn.undeads.com/assets/forum/currencies-bar/xt.png",
+      },
+      {
+        name: "Uniswap v3",
+        link: "https://app.uniswap.org",
+        description: "UDS / USDT",
+        icon: "https://cdn.undeads.com/assets/forum/currencies-bar/uniswap.png",
+      },
+    ];
   } catch (err) {
     console.error("[server-fetch] rates fetch error:", err);
     data.templateValues.externalResult = {error: true};
