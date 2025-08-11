eb7d689d7249189a2fd28b355f1c469a5e946627
Maxim Matsuk <86662841+mmatsuk-sc@users.noreply.github.com>
2025-08-11 15:25:30 +0300

    add backend api env (#1)
    Co-authored-by: Maxim <m.matsuk@applicaster.com>

--- library.js
+++ library.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const nconf = require("nconf");
+const apiData = nconf.get("api");
+
 const Plugin = {};
 
 Plugin.addExternalData = async function (data) {
@@ -58,9 +61,7 @@ Plugin.addExternalData = async function (data) {
 
   console.log("fetched rates", Date.now());
   try {
-    const response = await fetch(
-      "https://api-gateway.zombiesdev.com/rates/UDS/USD"
-    );
+    const response = await fetch(`${apiData.url}/rates/UDS/USD`);
     const json = await response.json();
 
     data.templateValues = data.templateValues || {};
